a0bb145a007bad11c68c4a33dc599dd615563a44
LuZASgc <qiebinghua058@126.com>
2021-04-11 10:28:59 +0800

    添加小时工基本信息，新建添加钟点工操作，并执行

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class AddEmployeeTest {
+public class AddEmployeeTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testAddHourlyEmployeeTransaction() {
+		int empId = 1001;
+		String name = "Bill";
+		String address = "Home";
+		double hourlyRate = 12.5;
+		
+		// 新建添加钟点工操作，并执行
+		Transaction t = new AddHourlyEmployeeTransaction(empId, name, address, hourlyRate);
+		t.execute();
 	}
 
 }
