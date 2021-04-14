654cc27df7bf21a4646697081c59eae0598e0161
LuZASgc <qiebinghua058@126.com>
2021-04-14 18:58:24 +0800

    赋值给新的字段

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -1,17 +1,29 @@
 package payroll.trans;
 
+import payroll.Employee;
+import payroll.PayrollDatabase;
 import payroll.Transaction;
 
 public class AddSalariedEmployeeTransaction implements Transaction {
 
-	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
-		// TODO Auto-generated constructor stub
+	private int empId;
+	private String name;
+	private String address;
+	private double salary;
+	public AddSalariedEmployeeTransaction(int empId, String name, 
+			String address, double salary) {
+				this.empId = empId;
+				this.name = name;
+				this.address = address;
+				this.salary = salary;
 	}
 
-	@Override
+	/*@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-
+		Employee employee = new Employee(empId,name,address);
+		employee.setPaymentClassification(new SalariedClassification(salary));
+		employee.setPaymentMethod(new HoldMethod());
+		PayrollDatabase.save(employee);
 	}
-
+*/
 }
